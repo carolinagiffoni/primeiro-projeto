9f38257345f4045744104d73389ba7b7fc16273b
Carol Giffoni <carolgiffoni@Daniels-MacBook-Air.local>
2022-11-30 21:42:39 -0300

    first commit

---
+++ src/Calculadora.java
@@ -0,0 +1,29 @@
+public class Calculadora {
+    public static void main(String[] args) {
+
+    }
+
+    void calcularSoma(){
+    }
+
+    void calcularSubtracao(){
+    }
+
+    void calcularDivisao(){
+    }
+
+    void calcularMultiplacao(){
+    }
+
+    void calcularExponencial(){
+    }
+
+    void calcularRaizQuandrada(){
+    }
+
+    void calcularPiso(){
+    }
+
+    void calcularTeto(){
+    }
+}
