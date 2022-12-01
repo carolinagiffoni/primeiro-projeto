c8068b0a87df3c05aa83b3c5c03355a97a2238dd
Carol Giffoni <carolgiffoni@Daniels-MacBook-Air.local>
2022-11-30 22:43:41 -0300

    implementa calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,29 +1,38 @@
+package util;
+
 public class Calculadora {
-    public static void main(String[] args) {
+	public double calcularSoma(double numeroA, double numeroB) {
+		return numeroA + numeroB;
+	}
+	
+	public double calcularSubtracao(double numeroA, double numeroB) {
+		return numeroA - numeroB;
+	}
+	
+	public double calcularDivisao(double numeroA, double numeroB) {
+		return numeroA / numeroB;
+	}
+	
+	public double calcularMultiplicacao(double numeroA, double numeroB) {
+		return numeroA * numeroB;
+	}
 
-    }
+	public double calcularRaizQuadrada(double numero) {
+		if (numero <= 0){
+			throw new RuntimeException("Não é possivel calcular raiz de número igual a zero ou negativo!");
+		}
+		return Math.sqrt(numero);
+	}
 
-    void calcularSoma(){
-    }
+	public double calcularPiso(double numero) {
+		return (int) Math.floor(numero);
+	}
 
-    void calcularSubtracao(){
-    }
+	public double calcularTeto (double numero) {
+		return (int) Math.ceil(numero);
+	}
 
-    void calcularDivisao(){
-    }
-
-    void calcularMultiplacao(){
-    }
-
-    void calcularExponencial(){
-    }
-
-    void calcularRaizQuandrada(){
-    }
-
-    void calcularPiso(){
-    }
-
-    void calcularTeto(){
-    }
+	public double calcularExponencial(double base, double expoente) {
+		return Math.pow(base, expoente);
+	}
 }
